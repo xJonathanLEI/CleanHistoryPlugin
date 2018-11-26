2d7c33d0bd0e5eaf2f4058c7588d5a9ec0f73069
Jonathan LEI <xJonathan@outlook.com>
2018-11-27 00:54:07 +0800

    Implemented history cleaning

--- clean_history_plugin.cpp
+++ clean_history_plugin.cpp
@@ -15,6 +15,55 @@ using boost::signals2::scoped_connection;
 
 static appbase::abstract_plugin &_clean_history_plugin = app().register_plugin<clean_history_plugin>();
 
+struct account_history_object : public chainbase::object<account_history_object_type, account_history_object>
+{
+    OBJECT_CTOR(account_history_object);
+
+    id_type id;
+    account_name account;             ///< the name of the account which has this action in its history
+    uint64_t action_sequence_num = 0; ///< the sequence number of the relevant action (global)
+    int32_t account_sequence_num = 0; ///< the sequence number for this account (per-account)
+};
+
+struct action_history_object : public chainbase::object<action_history_object_type, action_history_object>
+{
+
+    OBJECT_CTOR(action_history_object, (packed_action_trace));
+
+    id_type id;
+    uint64_t action_sequence_num; ///< the sequence number of the relevant action
+
+    shared_string packed_action_trace;
+    uint32_t block_num;
+    block_timestamp_type block_time;
+    transaction_id_type trx_id;
+};
+using account_history_id_type = account_history_object::id_type;
+using action_history_id_type = action_history_object::id_type;
+
+struct by_action_sequence_num;
+struct by_account_action_seq;
+struct by_trx_id;
+
+using action_history_index = chainbase::shared_multi_index_container<
+    action_history_object,
+    indexed_by<
+        ordered_unique<tag<by_id>, member<action_history_object, action_history_object::id_type, &action_history_object::id>>,
+        ordered_unique<tag<by_action_sequence_num>, member<action_history_object, uint64_t, &action_history_object::action_sequence_num>>,
+        ordered_unique<tag<by_trx_id>,
+                       composite_key<action_history_object,
+                                     member<action_history_object, transaction_id_type, &action_history_object::trx_id>,
+                                     member<action_history_object, uint64_t, &action_history_object::action_sequence_num>>>>>;
+
+using account_history_index = chainbase::shared_multi_index_container<
+    account_history_object,
+    indexed_by<
+        ordered_unique<tag<by_id>, member<account_history_object, account_history_object::id_type, &account_history_object::id>>,
+        ordered_unique<tag<by_account_action_seq>,
+                       composite_key<account_history_object,
+                                     member<account_history_object, account_name, &account_history_object::account>,
+                                     member<account_history_object, int32_t, &account_history_object::account_sequence_num>>>>>;
+
 class clean_history_plugin_impl
 {
 }; // namespace eosio
@@ -28,6 +77,34 @@ void clean_history_plugin::plugin_initialize(const variables_map &options)
 {
     try
     {
+        auto *chain_plug = app().find_plugin<chain_plugin>();
+        EOS_ASSERT(chain_plug, chain::missing_chain_plugin_exception, "");
+        auto &chain = chain_plug->chain();
+        chainbase::database &db = const_cast<chainbase::database &>(chain.db()); // Override read-only access to state DB (highly unrecommended practice!)
+
+        uint64_t rows_erased = 0;
+
+        // Cleans action
+        std::cout << "Cleaning action history..." << std::endl;
+        const auto &action_idx = db.get_index<action_history_index, by_id>();
+        auto &mutable_action_idx = db.get_mutable_index<action_history_index>();
+        while (!action_idx.empty())
+        {
+            mutable_action_idx.remove(*action_idx.begin());
+            rows_erased++;
+        }
+        std::cout << "Action history rows removed: " << rows_erased << std::endl;
+
+        // Cleans account
+        std::cout << "Cleaning account history..." << std::endl;
+        const auto &account_idx = db.get_index<account_history_index, by_id>();
+        auto &mutable_account_idx = db.get_mutable_index<account_history_index>();
+        while (!account_idx.empty())
+        {
+            mutable_account_idx.remove(*account_idx.begin());
+            rows_erased++;
+        }
+        std::cout << "Account history rows removed: " << rows_erased << std::endl;
     }
     FC_LOG_AND_RETHROW()
 }
